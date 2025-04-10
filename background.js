8748c36bb2d45b389e544aaa36dedfdc18406d12
LDcourrielleur <93119484+LDcourrielleur@users.noreply.github.com>
2025-04-10 11:44:26 +0200

    Mise en place des logos et de l'installation

--- background.js
+++ background.js
@@ -1,37 +1,23 @@
-//var paulineUrl = "https://annuaire-preprod.e2.rie.gouv.fr/";
-var paulineUrl = "https://annuaire-preprod.e2.rie.gouv.fr?courrielleur=true"
-
 // --------------- SPACE TOOLBAR BUTTON ------
 async function createSpaceButton() {
   try {
-    const spaceName = "Pauline";
-    const defaultUrl = browser.runtime.getURL("content/pauline.html");
+    const spaceName = "Archibald";
+    const defaultUrl = browser.runtime.getURL("content/archibald.html");
     const buttonProperties = {
-      title: "Contacts ministériels",
+      title: "Archivage de boîte à lettres",
       defaultIcons: {
-        "18": "skin/images/logo-18.png",
-        "32": "skin/images/logo-32.png"
+        "24": {
+          "light": "skin/images/archibald_logo_24_light.png",
+          "dark": "skin/images/archibald_logo_24_dark.png"
+        }
       }
     };
 
     const space = await browser.spaces.create(spaceName, defaultUrl, buttonProperties);
-    console.log(`Pauline button created with space ID: ${space.id}`);
+    console.log(`Archibald button created with space ID: ${space.id}`);
   } catch (error) {
     console.error("Error creating space:", error);
   }
 }
 createSpaceButton();
-// -------------------------------------------
-
-// ----------- COMPOSE MAIL BUTTON -----------
-async function openPauline(composeWindowId) {
-  // TODO: use composeWindowId to add mail only to current window
-  let composePaulineUrl = messenger.extension.getURL("content/pauline.html")+ "?compose=true";
-  console.log(composePaulineUrl);
-  await messenger.windows.create({'type': 'popup', 'url': composePaulineUrl });
-}
-
-messenger.composeAction.onClicked.addListener(async (tab) => {
-  openPauline(tab.windowId);
-});
 // -------------------------------------------
